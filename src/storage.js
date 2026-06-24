5bfd3e624f84c847e6b120371612d68ad583fa89
stellamaris444 <stellamaris444.am@gmail.com>
2026-06-24 16:32:02 +0900

    Safe require for tasks.js

--- src/storage.js
+++ src/storage.js
@@ -9,6 +9,10 @@ function load() {
   return { userId: null, doneTasks: [], lastActivity: null, customTodayTasks: null, customHabitTasks: null, morningMessageSentAt: null, lastResponseAt: null, yesterdayIncompleteTasks: [] };
 }
 
+function loadDefaultTasks() {
+  try { return require("../tasks"); } catch { return { todayTasks: {}, habitTasks: {} }; }
+}
+
 function save(data) { fs.writeFileSync(FILE, JSON.stringify(data, null, 2), "utf8"); }
 function getState() { return load(); }
 function setUserId(id) { const s = load(); s.userId = id; save(s); }
@@ -16,9 +20,10 @@ function markTaskDone(name) { const s = load(); if (!s.doneTasks.includes(name))
 
 function resetDaily() {
   const s = load();
+  const defaults = loadDefaultTasks();
   const allTasks = [];
-  const today = s.customTodayTasks || require("../tasks").todayTasks;
-  const habit = s.customHabitTasks || require("../tasks").habitTasks;
+  const today = s.customTodayTasks || defaults.todayTasks;
+  const habit = s.customHabitTasks || defaults.habitTasks;
   for (const list of Object.values({ ...today, ...habit })) {
     for (const task of list) allTasks.push(task);
   }
@@ -33,12 +38,12 @@ function updateActivity() { const s = load(); s.lastActivity = new Date().toISOS
 function getTodayTasks() {
   const s = load();
   if (s.customTodayTasks && Object.keys(s.customTodayTasks).length > 0) return s.customTodayTasks;
-  return require("../tasks").todayTasks;
+  return loadDefaultTasks().todayTasks;
 }
 function getHabitTasks() {
   const s = load();
   if (s.customHabitTasks && Object.keys(s.customHabitTasks).length > 0) return s.customHabitTasks;
-  return require("../tasks").habitTasks;
+  return loadDefaultTasks().habitTasks;
 }
 function setTodayTasks(t) { const s = load(); s.customTodayTasks = t; save(s); }
 function setHabitTasks(t) { const s = load(); s.customHabitTasks = t; save(s); }
@@ -59,7 +64,6 @@ function hasRespondedSinceMorning() {
   return new Date(s.lastResponseAt) > new Date(s.morningMessageSentAt);
 }
 
-// 最後の活動から1時間以上経過しているか
 function hasBeenInactiveFor1Hour() {
   const s = load();
   if (!s.lastActivity) return true;
